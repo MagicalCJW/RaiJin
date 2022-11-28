d6fb267c6fcd78749c74b1f44db9026403231e9c
elemmire <elemmireck@gmail.com>
2022-11-28 21:29:35 +0800

    Mappings for redirecting requests and HttpClient

---
+++ src/main/java/org/RaiJin/core/config/MappingProperties.java
@@ -0,0 +1,80 @@
+package org.RaiJin.core.config;
+
+import lombok.Data;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.commons.lang3.builder.ToStringStyle.NO_CLASS_NAME_STYLE;
+
+
+@Data
+public class MappingProperties {
+
+    private String name;
+
+    private String host = "";
+
+    private List<String> destinations = new ArrayList<>();
+
+    private TimeOutProperties timeout = new TimeOutProperties();
+
+    private Map<String, Object> customConfiguration = new HashMap<>();
+
+    public MappingProperties copy() {
+        MappingProperties properties = new MappingProperties();
+        properties.setName(name);
+        properties.setHost(host);
+        properties.setTimeout(timeout);
+        properties.setDestinations(destinations==null?null:new ArrayList<>(destinations));
+        properties.setCustomConfiguration(customConfiguration==null?null:new HashMap<>(customConfiguration));
+
+        return properties;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, NO_CLASS_NAME_STYLE)
+                .append("name", name)
+                .append("host", host)
+                .append("destinations", destinations)
+                .append("timeout", timeout)
+                .append("customConfiguration", customConfiguration)
+                .toString();
+    }
+
+    public static class TimeOutProperties {
+        private int connect = 2000;
+        private int read = 20000;
+
+        public int getConnect() {
+            return connect;
+        }
+
+        public void setConnect(int connect) {
+            this.connect = connect;
+        }
+
+        public int getRead() {
+            return read;
+        }
+
+        public void setRead(int read) {
+            this.read = read;
+        }
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this, ToStringStyle.NO_CLASS_NAME_STYLE)
+                    .append("connect", connect)
+                    .append("read", read)
+                    .toString();
+        }
+    }
+}
+
+
---
+++ src/main/java/org/RaiJin/core/http/HttpClient.java
@@ -0,0 +1,43 @@
+package org.RaiJin.core.http;
+
+import org.RaiJin.core.config.MappingProperties;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ *
+ */
+
+public class HttpClient {
+
+    protected Map<String, RestTemplate> httpClients = new HashMap<>();
+
+    public RestTemplate getHttpClient(String name) {
+        return httpClients.get(name);
+    }
+
+    public void updateHttpClients(List<MappingProperties> mappingPropertiesList) {
+        this.httpClients = mappingPropertiesList.stream().collect(Collectors.toMap(MappingProperties::getName, this::createRestTemplate));
+    }
+
+    protected RestTemplate createRestTemplate(MappingProperties mappingProperties) {
+        CloseableHttpClient client = createHttpClient().build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(client);
+        requestFactory.setConnectTimeout(mappingProperties.getTimeout().getConnect());
+        requestFactory.setReadTimeout(mappingProperties.getTimeout().getRead());
+
+        return new RestTemplate(requestFactory);
+    }
+
+    protected HttpClientBuilder createHttpClient() {
+        return HttpClientBuilder.create().useSystemProperties().disableRedirectHandling().disableCookieManagement();
+    }
+}

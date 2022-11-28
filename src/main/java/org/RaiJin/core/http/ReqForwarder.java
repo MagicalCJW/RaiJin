c35616f0aef5a24e4a296e079855d6b1de594143
elemmire <elemmireck@gmail.com>
2022-11-28 21:31:26 +0800

    create ReqForwarder class

---
+++ src/main/java/org/RaiJin/core/http/ReqForwarder.java
@@ -0,0 +1,10 @@
+package org.RaiJin.core.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReqForwarder {
+
+    private static final Logger log = LoggerFactory.getLogger(ReqForwarder.class);
+
+}

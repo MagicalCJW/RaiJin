4b5372cc60891b660e998ffddeadb70382f5335f
elemmire <351059171@qq.com>
2022-12-01 17:08:41 +0800

    add implementation of ProgrammaticMappingsProvider

--- src/main/java/org/RaiJin/core/mappings/ProgrammaticMappingsProvider.java
+++ src/main/java/org/RaiJin/core/mappings/ProgrammaticMappingsProvider.java
@@ -1,18 +1,23 @@
 package org.RaiJin.core.mappings;
 
+import org.RaiJin.common.env.EnvConfig;
+import org.RaiJin.common.services.Service;
+import org.RaiJin.common.services.ServiceDirectory;
 import org.RaiJin.config.MappingProperties;
 import org.RaiJin.config.RaiJinProperties;
 import org.RaiJin.core.http.HttpClient;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ProgrammaticMappingsProvider extends MappingsProvider{
 
-    public ProgrammaticMappingsProvider(ServerProperties serverProperties, RaiJinProperties raiJinProperties, MappingValidator mappingValidator, HttpClient httpClient) {
+    protected final EnvConfig envConfig;
+
+    public ProgrammaticMappingsProvider(EnvConfig envConfig,ServerProperties serverProperties, RaiJinProperties raiJinProperties, MappingValidator mappingValidator, HttpClient httpClient) {
         super(serverProperties, raiJinProperties, mappingValidator, httpClient);
+        this.envConfig = envConfig;
     }
 
     @Override
@@ -23,7 +28,18 @@ public class ProgrammaticMappingsProvider extends MappingsProvider{
     @Override
     protected List<MappingProperties> retrieveMappings() {
         List<MappingProperties> mappings = new ArrayList<>();
-//        Map<String, ?> serviceMap =
+        Map<String, Service> serviceMap = ServiceDirectory.getMapping();
+        for(String key: serviceMap.keySet()) {
+            String subDomain = key.toLowerCase();
+            Service service = serviceMap.get(key);
+            MappingProperties mapping = new MappingProperties();
+            mapping.setName(subDomain+"_route");
+            mapping.setHost(subDomain+"."+envConfig.getExternalApex());
+            // No security on backend right now :-(
+            String dest = "http://"+service.getBackendDomain();
+            mapping.setDestinations(List.of(dest));
+            mappings.add(mapping);
+        }
         return mappings;
     }
 }

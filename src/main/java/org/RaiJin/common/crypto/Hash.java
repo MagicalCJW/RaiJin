733b952edac2a16f13aa3e3bfe7c1c15301c6b21
elemmire <elemmireck@gmail.com>
2022-12-02 15:08:53 +0800

    add secret encode method and its dependency

---
+++ src/main/java/org/RaiJin/common/crypto/Hash.java
@@ -0,0 +1,17 @@
+package org.RaiJin.common.crypto;
+
+import org.bouncycastle.util.encoders.Hex;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+public class Hash {
+    public static String encode(String key, String data) throws Exception {
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8),"HmacSHA256");
+        sha256_HMAC.init(secretKey);
+
+        return Hex.toHexString(sha256_HMAC.doFinal(data.getBytes(StandardCharsets.UTF_8)));
+    }
+}

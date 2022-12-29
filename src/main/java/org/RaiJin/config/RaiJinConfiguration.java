3d4b4b2a70e29bc28e5e49cdc48cc7d637b1b2ab
elemmire <elemmireck@gmail.com>
2022-12-29 15:02:22 +0800

    add so many ConditionalOnMissingBean to prevent bean missing

--- src/main/java/org/RaiJin/config/RaiJinConfiguration.java
+++ src/main/java/org/RaiJin/config/RaiJinConfiguration.java
@@ -2,10 +2,27 @@ package org.RaiJin.config;
 
 import org.RaiJin.common.config.ItachiWebConfig;
 import org.RaiJin.common.env.EnvConfig;
+import org.RaiJin.core.balancer.LoadBalancer;
+import org.RaiJin.core.balancer.RandomLoadBalancer;
+import org.RaiJin.core.filter.FaviconFilter;
 import org.RaiJin.core.filter.NakedDomainFilter;
+import org.RaiJin.core.filter.SecurityFilter;
+import org.RaiJin.core.http.HttpClient;
+import org.RaiJin.core.http.ReqForwarder;
+import org.RaiJin.core.http.RequestDataExtractor;
 import org.RaiJin.core.http.ReverseProxyFilter;
+import org.RaiJin.core.interceptor.CacheResponseInterceptor;
+import org.RaiJin.core.interceptor.LoggingTraceInterceptor;
+import org.RaiJin.core.interceptor.PostForwardResponseInterceptor;
+import org.RaiJin.core.interceptor.PreForwardRequestInterceptor;
+import org.RaiJin.core.mappings.ConfigurationMappingsProvider;
+import org.RaiJin.core.mappings.MappingValidator;
+import org.RaiJin.core.mappings.MappingsProvider;
+import org.RaiJin.core.mappings.ProgrammaticMappingsProvider;
+import org.RaiJin.core.trace.ProxyingTraceInterceptor;
+import org.RaiJin.core.trace.TraceInterceptor;
 import org.RaiJin.view.AssetLoader;
-import org.apache.catalina.Server;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -13,6 +30,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.Ordered;
+import io.micrometer.core.instrument.MeterRegistry;
+
+import java.util.Optional;
 
 @Configuration
 @EnableConfigurationProperties({RaiJinProperties.class, ItachiProperties.class})
@@ -49,5 +69,100 @@ public class RaiJinConfiguration {
         return registrationBean;
     }
 
+    @Bean
+    public FilterRegistrationBean<SecurityFilter> securityFilterFilterRegistrationBean(EnvConfig envConfig) {
+        FilterRegistrationBean<SecurityFilter> registrationBean =
+                new FilterRegistrationBean<>(new SecurityFilter(envConfig));
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE+80);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<FaviconFilter> faviconFilterFilterRegistrationBean()
+    {
+        FilterRegistrationBean<FaviconFilter> registrationBean =
+                new FilterRegistrationBean<>(new FaviconFilter(assetLoader.getFaviconBytes()));
+                registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE+70);
+                return registrationBean;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ReverseProxyFilter raiJinReverseProxyFilter(
+            RequestDataExtractor extractor,
+            MappingsProvider provider,
+            ReqForwarder forwarder,
+            ProxyingTraceInterceptor interceptor,
+            PreForwardRequestInterceptor requestInterceptor
+    ) {
+        return new ReverseProxyFilter(raiJinProperties,extractor,provider,forwarder,interceptor,requestInterceptor);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public HttpClient raiJinHttpClientProvider() {
+        return new HttpClient();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public RequestDataExtractor raiJinRequestDataExtractor() {
+        return new RequestDataExtractor();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MappingsProvider raiJinConfigurationMappingsProvider(EnvConfig envConfig,
+                                                                MappingValidator validator,
+                                                                HttpClient httpClientProvider) {
+        if(raiJinProperties.isEnableProgrammaticMapping()) {
+            return new ProgrammaticMappingsProvider(envConfig,serverProperties,raiJinProperties,validator,httpClientProvider);
+        } else {
+            return new ConfigurationMappingsProvider(serverProperties, raiJinProperties, validator, httpClientProvider);
+        }
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public LoadBalancer raiJinLoadBalancer() {
+        return new RandomLoadBalancer();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MappingValidator raiJinMappingsValidator() {
+        return new MappingValidator();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ReqForwarder raiJinRequestForwarder(
+            HttpClient httpClient,
+            MappingsProvider provider,
+            LoadBalancer loadBalancer,
+            Optional<MeterRegistry> meterRegistry,
+            ProxyingTraceInterceptor traceInterceptor,
+            PostForwardResponseInterceptor responseInterceptor
+    ) {
+        return new ReqForwarder(serverProperties,raiJinProperties,httpClient,provider,loadBalancer,meterRegistry,traceInterceptor,responseInterceptor);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public TraceInterceptor raiJinTraceInterceptor() {
+        return new LoggingTraceInterceptor();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ProxyingTraceInterceptor raiJinProxyingTraceInterceptor(TraceInterceptor traceInterceptor) {
+        return new ProxyingTraceInterceptor(raiJinProperties, traceInterceptor);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public PostForwardResponseInterceptor raiJinPostForwardResponseInterceptor() {
+        return new CacheResponseInterceptor();
+    }
 
 }
